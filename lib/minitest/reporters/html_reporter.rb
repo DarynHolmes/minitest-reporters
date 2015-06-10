e768edabdaf405e4d1928cdcada040d1ba1b9315
Daryn Holmes <daryn.holmes@net-a-porter.com>
2015-06-10 18:02:51 +0100

    Cleaning up

--- lib/minitest/reporters/html_reporter.rb
+++ lib/minitest/reporters/html_reporter.rb
@@ -5,9 +5,12 @@ require 'awesome_print'
 module Minitest
   module Reporters
     class HtmlReporter < BaseReporter
-      def initialize(reports_dir = "test/html_reports", empty = true)
-        @report_title = 'Fulcrum Specs'
+
+      attr_reader :title
+
+      def initialize(title = 'Test Results', reports_dir = "test/html_reports", empty = true)
         super({})
+        @title = title
         @reports_path = File.absolute_path(reports_dir)
 
         if empty
@@ -17,83 +20,91 @@ module Minitest
         end
       end
 
-      def passes
-        count - failures - errors - skips
-      end
-
       def report
         super
 
         puts "Writing HTML reports to #{@reports_path}"
-        erb_file = 'lib/minitest/templates/index.html.erb'
+        erb_file = "#{File.dirname(__FILE__)}/../templates/index.html.erb"
         html_file = @reports_path + "/index.html"
         erb_str = File.read(erb_file)
         renderer = ERB.new(erb_str)
 
-        tests_by_groups = tests.group_by(&:class) # taken from the JUnit reporter
-        suites = []
-        tests_by_groups.each do |suite, tests|
-          suite_result = analyze_suite(tests)
-          suite_result[:name] = suite.to_s
-          suite_result[:tests] = []
-          tests.each do |test|
-            test_map = {}
-            test_map[:name] = friendly_name(test)
-            test_map[:classname] = suite.to_s
-            test_map[:assertion_count] = test.assertions
-            test_map[:time] = test.time
-            test_map[:result] = result(test)
-            test_map[:message] = test.failure.message unless test.passed?
-            test_map[:location] = location(test.failure) unless test.passed?
+        tests_by_suites = tests.group_by(&:class) # taken from the JUnit reporter
 
-            suite_result[:tests] << test_map
-          end
-          suite_result[:tests].sort! { |a, b| compare_tests(a, b) }
-          suites << suite_result
+        suites = tests_by_suites.map do |suite, tests|
+          suite_summary = summarize_suite(suite, tests)
+          suite_summary[:tests] = tests.sort { |a, b| compare_tests(a, b) }
+          suite_summary
         end
 
-        # suites.sort! { |a, b| a[:name].to_s <=> b[:name].to_s }
+        # tests_by_suites.sort! { |a, b| a[:name].to_s <=> b[:name].to_s }
         suites.sort! { |a, b| compare_suites(a, b) }
 
         result = renderer.result(binding)
-
         File.open(html_file, 'w') do |f|
           f.write(result)
         end
       end
 
-      private
+      # .map do |test|
+      #   ap test
+      #   ap "---"
+      #   test_map = {}
+      #   test_map[:name] = friendly_name(test)
+      #   test_map[:classname] = suite.to_s
+      #   test_map[:assertion_count] = test.assertions
+      #   test_map[:time] = test.time
+      #   test_map[:result] = result(test)
+      #   test_map[:message] = test.failure.message unless test.passed?
+      #   test_map[:location] = location(test.failure) unless test.passed?
+      #   test_map
+      # end
+      #suite_summary[:tests].sort! { |a, b| compare_tests(a, b) }
 
-      def compare_suites(suite_a, suite_b)
-        return 0 if suite_has_errors_or_failures(suite_a) && suite_has_errors_or_failures(suite_b)
-        return -1 if suite_has_errors_or_failures(suite_a) && !suite_has_errors_or_failures(suite_b)
-        return 1 if !suite_has_errors_or_failures(suite_a) && suite_has_errors_or_failures(suite_b)
+      def passes
+        count - failures - errors - skips
+      end
 
-        return 0 if suite_has_skipps(suite_a) && suite_has_skipps(suite_b)
-        return -1 if suite_has_skipps(suite_a) && !suite_has_skipps(suite_b)
-        return 1 if !suite_has_skipps(suite_a) && suite_has_skipps(suite_b)
+      # private
 
+      def compare_suites_by_name(suite_a, suite_b)
         suite_a[:name] <=> suite_b[:name]
       end
 
+      def compare_suites(suite_a, suite_b)
+        return compare_suites_by_name(suite_a, suite_b) if suite_has_errors_or_failures(suite_a) && suite_has_errors_or_failures(suite_b)
+        return -1 if suite_has_errors_or_failures(suite_a) && !suite_has_errors_or_failures(suite_b)
+        return 1 if !suite_has_errors_or_failures(suite_a) && suite_has_errors_or_failures(suite_b)
+
+        return compare_suites_by_name(suite_a, suite_b) if suite_has_skipps(suite_a) && suite_has_skipps(suite_b)
+        return -1 if suite_has_skipps(suite_a) && !suite_has_skipps(suite_b)
+        return 1 if !suite_has_skipps(suite_a) && suite_has_skipps(suite_b)
+
+        compare_suites_by_name(suite_a, suite_b)
+      end
+
+      def compare_tests_by_name(test_a, test_b)
+        friendly_name(test_a) <=> friendly_name(test_b)
+      end
+
       def compare_tests(test_a, test_b)
-        return 0 if test_failed(test_a) && test_failed(test_b)
-        return -1 if test_failed(test_a) && !test_failed(test_b)
-        return 1 if !test_failed(test_a) && test_failed(test_b)
+        return compare_tests_by_name(test_a, test_b) if test_failed?(test_a) && test_failed?(test_b)
+        return -1 if test_failed?(test_a) && !test_failed?(test_b)
+        return 1 if !test_failed?(test_a) && test_failed?(test_b)
 
-        return 0 if test_skipped(test_a) && test_skipped(test_b)
-        return -1 if test_skipped(test_a) && !test_skipped(test_b)
-        return 1 if !test_skipped(test_a) && test_skipped(test_b)
+        return compare_tests_by_name(test_a, test_b) if test_skipped?(test_a) && test_skipped?(test_b)
+        return -1 if test_skipped?(test_a) && !test_skipped?(test_b)
+        return 1 if !test_skipped?(test_a) && test_skipped?(test_b)
 
-        test_a[:name] <=> test_b[:name]
+        friendly_name(test_a) <=> friendly_name(test_b)
       end
 
-      def test_failed(test)
-        test[:result] == :error || test[:result] == :fail
+      def test_failed?(test)
+        test.error? || test.failure
       end
 
-      def test_skipped(test)
-        test[:result] == :skip
+      def test_skipped?(test)
+        test.skipped?
       end
 
       def suite_has_skipps(suite)
@@ -111,19 +122,20 @@ module Minitest
         "it #{groups[0][1]}"
       end
 
-# taken from the JUnit reporter
-      def analyze_suite(tests)
-        result = Hash.new(0)
+      # based on analyze_suite from the JUnit reporter
+      def summarize_suite(suite, tests)
+        summary = Hash.new(0)
+        summary[:name] = suite.to_s
         tests.each do |test|
-          result[:"#{result(test)}_count"] += 1
-          result[:assertion_count] += test.assertions
-          result[:test_count] += 1
-          result[:time] += test.time
+          summary[:"#{result(test)}_count"] += 1
+          summary[:assertion_count] += test.assertions
+          summary[:test_count] += 1
+          summary[:time] += test.time
         end
-        result
+        summary
       end
 
-# based on message_for(test) from the JUnit reporter
+      # based on message_for(test) from the JUnit reporter
       def message_for(test)
         suite = test.class
         name = test.name
